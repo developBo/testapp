b3abb99bf91ff8f0836827d705a035ec31201385
developBo <developBo@naver.com>
2021-03-09 10:59:54 +0900

    first push

---
+++ src/main/java/com/ict/testapp/model/service/MemberService.java
@@ -0,0 +1,15 @@
+package com.ict.testapp.model.service;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+
+@Service("service")
+public class MemberService {
+
+	public int insertMember(HashMap<String, String> paraMap) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
